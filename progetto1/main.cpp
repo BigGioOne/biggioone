79821cfbba6c904708c52358b80e4878bb65f1bf
BiGioOne <biggioone@gmail.com>
2017-12-01 16:36:08 +0100

    Primo Commit

---
+++ progetto1/main.cpp
@@ -0,0 +1,40 @@
+/*
+ * senza nome.cxx
+ * 
+ * Copyright 2013 giovanni <giovanni@giovanni-linux>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <cstdio>
+#include <cmath>
+
+int main()
+{
+	float a=0; float b=0;
+	 	
+	a=3.5 ; b=7.8;
+	
+	printf("%f", a * b);
+	
+	getchar();
+	
+	return 0;
+}
+
